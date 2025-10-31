92206e4336f549e0de8b8a022e9d860f27febc6e
CupcakePolish <90985312+CupcakePolish@users.noreply.github.com>
2025-10-31 19:59:13 +0100

    Update logout.js

--- functions/logout.js
+++ functions/logout.js
@@ -1,7 +1,8 @@
 // /functions/logout.js
 export async function onRequestGet({ request }) {
   const url = new URL(request.url);
-  const returnTo = `${url.origin}/`; // go to home after logout
-  const target = `/cdn-cgi/access/logout?return_to=${encodeURIComponent(returnTo)}`;
+  const afterLogin = `${url.origin}/`; // where to go after re-login
+  const loginUrl = `/cdn-cgi/access/login?return_to=${encodeURIComponent(afterLogin)}`;
+  const target = `/cdn-cgi/access/logout?return_to=${encodeURIComponent(loginUrl)}`;
   return Response.redirect(target, 302);
 }
